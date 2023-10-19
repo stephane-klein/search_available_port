0058827a407ddd1ea8776637a59a8a16c1cc8d56
Stéphane Klein <contact@stephane-klein.info>
2023-10-19 16:08:05 +0200

    Improve search_available_port.py

--- search_available_port.py
+++ search_available_port.py
@@ -39,21 +39,29 @@ with open(args.state_file + ".lock", 'w') as lock_file:
         with open(args.state_file, "r") as reader:
             state_dict = json.loads(reader.read())
 
-    available_port = None
-    reserved_ports = state_dict.values()
-    for port in range(int(args._from), int(args._from) + 400):
-        if port not in reserved_ports:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                res = sock.connect_ex(("localhost", port))
-                if res != 0:
-                    print("Free port: %s" % port)
-                    available_port = port
-                    break
+    if args.name not in state_dict:
+        available_port = None
+        reserved_ports = state_dict.values()
+        for port in range(int(args._from), int(args._from) + 400):
+            if port not in reserved_ports:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    res = sock.connect_ex(("localhost", port))
+                    if res != 0:
+                        print("Free port: %s" % port)
+                        available_port = port
+                        break
 
-    state_dict[args.name] = available_port
+        state_dict[args.name] = available_port
 
-    with open(args.state_file, "w") as writer:
-        writer.write(json.dumps(state_dict, indent=4))
+        with open(args.state_file, "w") as writer:
+            writer.write(json.dumps(state_dict, indent=4))
+    else:
+        print(
+            "%s port already reserved for %s instance, no change made" % (
+                state_dict[args.name],
+                args.name
+            )
+        )
 
     fcntl.flock(lock_file, fcntl.LOCK_UN)
 
